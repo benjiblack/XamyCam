9234ff59dd3c9fa90603a01cf28c7e04f7107473
Ben Black <ben@cds.digital>
2018-07-31 17:13:06 -0500

    Initial setup.
    - Add XamyCam.iOS Demo project.
    - Setup demo navigation.
    - Add CameraViewController.

---
+++ XamyCam.iOS/CameraViewController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UIKit;
+
+namespace XamyCam.iOS
+{
+    public class CameraViewController : UIViewController
+    {
+        public CameraViewController()
+        {
+            View.BackgroundColor = UIColor.Black;
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+        }
+
+        public override void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
+        }
+    }
+}
---
+++ XamyXam.Demo.iOS/ViewController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using XamyCam.iOS;
+using UIKit;
+
+namespace XamyXam.Demo.iOS
+{
+    public partial class ViewController : UIViewController
+    {
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
+
+            var cameraViewController = new CameraViewController();
+            PresentViewController(cameraViewController, true, null);
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
